41c10b0075e92b3a269910aadbc539d46b5f6c00
Ebubekir <Ebubekirustalar@gmail.com>
2021-11-23 13:45:20 +0300

    FBA send suggestion fuctionality added

--- test_app_1/admin.py
+++ test_app_1/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import SKU, Warehouse, Purchase_Order, Plain_Carton_Line_Item
 
 # Register your models here.
+
+
+class SKUAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General information', {'fields': ['id','sku','required_pcs_fba_send_in_GERMANY', 'required_pcs_fba_send_in_FRANCE']}),
+    ]
+    list_display = ('id','sku','required_pcs_fba_send_in_GERMANY', 'required_pcs_fba_send_in_FRANCE')
+
+class WarehouseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General information', {'fields': ['id','warehouse_name']}),
+    ]
+    list_display = ('id','warehouse_name')
+
+class Purchase_OrderAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General information', {'fields': ['id','order_name','warehouse', 'status']}),
+    ]
+    list_display = ('id','order_name','warehouse', 'status')
+
+class Plain_Carton_Line_ItemAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General information', {'fields': ['id','purchase_order','qty_cartons', 'cartons_left_cached', 'sku_obj', 'pcs_per_carton']}),
+    ]
+    list_display = ('id','purchase_order','qty_cartons', 'cartons_left_cached', 'sku_obj', 'pcs_per_carton')
+    
+admin.site.register(SKU, SKUAdmin)
+admin.site.register(Warehouse, WarehouseAdmin)
+admin.site.register(Purchase_Order, Purchase_OrderAdmin)
+admin.site.register(Plain_Carton_Line_Item, Plain_Carton_Line_ItemAdmin)
--- test_app_1/views.py
+++ test_app_1/views.py
@@ -3,17 +3,56 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from test_app_1.models import Warehouse
-
+from test_app_1.models import Warehouse, SKU, Purchase_Order, Plain_Carton_Line_Item
 
 def index(request):
     return render(request, 'test_page.html')
 
+def generate_suggestions(output, required_pcs_fba_send_number, country, item, check_repeat):
+    if country not in output.keys():
+        output[country] = {}
+        output[country]["source_warehouses"] = {}
+
+    if item.purchase_order.warehouse.id not in output[country]["source_warehouses"].keys():
+        output[country]["source_warehouses"][item.purchase_order.warehouse.id] = {}
+        output[country]["source_warehouses"][item.purchase_order.warehouse.id]["carton_qty_for_matrix"] = 0
+        output[country]["source_warehouses"][item.purchase_order.warehouse.id]["skus_that_need_to_be_send"] = {}
+
+    needed_carton_count_for_sku = int(required_pcs_fba_send_number / item.pcs_per_carton + 0.5)
+    qty_cartons_in_plan = 0
+    if needed_carton_count_for_sku >= item.cartons_left_cached:
+        qty_cartons_in_plan = item.cartons_left_cached
+    else:
+        qty_cartons_in_plan = needed_carton_count_for_sku
+
+    if output[country]["source_warehouses"][item.purchase_order.warehouse.id]["carton_qty_for_matrix"] + qty_cartons_in_plan >= needed_carton_count_for_sku:
+        qty_cartons_in_plan = needed_carton_count_for_sku - output[country]["source_warehouses"][item.purchase_order.warehouse.id]["carton_qty_for_matrix"]
+
+    if check_repeat and item.sku_obj.id in output["amazon.de"]["source_warehouses"][item.purchase_order.warehouse.id]["skus_that_need_to_be_send"] and item.id in output["amazon.de"]["source_warehouses"][item.purchase_order.warehouse.id]["skus_that_need_to_be_send"][item.sku_obj.id]["plain_carton_line_items"]:
+        qty_cartons_in_plan = output["amazon.de"]["source_warehouses"][item.purchase_order.warehouse.id]["skus_that_need_to_be_send"][item.sku_obj.id]["plain_carton_line_items"][item.id]["qty_cartons_in_plan"] - item.cartons_left_cached
+    if qty_cartons_in_plan > 0:
+        if item.sku_obj.id not in output[country]["source_warehouses"][item.purchase_order.warehouse.id]["skus_that_need_to_be_send"]:
+            output[country]["source_warehouses"][item.purchase_order.warehouse.id]["skus_that_need_to_be_send"][item.sku_obj.id] = {}
+            output[country]["source_warehouses"][item.purchase_order.warehouse.id]["skus_that_need_to_be_send"][item.sku_obj.id]["plain_carton_line_items"] = {}
+        output[country]["source_warehouses"][item.purchase_order.warehouse.id]["carton_qty_for_matrix"] += qty_cartons_in_plan
+        output[country]["source_warehouses"][item.purchase_order.warehouse.id]["skus_that_need_to_be_send"][item.sku_obj.id]["plain_carton_line_items"][item.id]= {"id":item.id, "qty_cartons_in_plan": qty_cartons_in_plan}
+
+    return output
+
+def fba_send_in_suggestions():
+    output = {}
+    plain_carton_line_items = Plain_Carton_Line_Item.objects.filter(cartons_left_cached__gt = 0, purchase_order__status="Received")
+    for plain_carton_line_item in plain_carton_line_items:
+        if plain_carton_line_item.sku_obj.required_pcs_fba_send_in_GERMANY > 0:
+            output = generate_suggestions(output, plain_carton_line_item.sku_obj.required_pcs_fba_send_in_GERMANY, "amazon.de", plain_carton_line_item, check_repeat=False)
+        if plain_carton_line_item.sku_obj.required_pcs_fba_send_in_FRANCE > 0:
+            output = generate_suggestions(output, plain_carton_line_item.sku_obj.required_pcs_fba_send_in_FRANCE, "amazon.fr", plain_carton_line_item, check_repeat=True)
+    return output
 
 def ajax_get_table_data(request):
     response_dict = []
     action = request.POST.get('action', '')
-
+    suggestions = fba_send_in_suggestions()
 
     if action == "dt_sugg_fba_send_ins":
         warehouses = Warehouse.objects.all()
@@ -21,8 +60,8 @@ def ajax_get_table_data(request):
             response_dict.append({
                 "warehouse_id": wh.id,
                 "warehouse": wh.warehouse_name,
-                "amazon_de": 0,
-                "amazon_fr": 1,
+                "amazon_de": suggestions["amazon.de"]["source_warehouses"][wh.id]["carton_qty_for_matrix"],
+                "amazon_fr": suggestions["amazon.fr"]["source_warehouses"][wh.id]["carton_qty_for_matrix"],
 
             })
 
